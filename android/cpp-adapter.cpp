3b4a8b152916debad8b864a42f3a157333717fd5
Berend Sliedrecht <berend@animo.id>
2022-02-23 12:01:13 +0100

    android working and pushing the libraries
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- android/cpp-adapter.cpp
+++ android/cpp-adapter.cpp
@@ -1,8 +1,38 @@
 #include <jni.h>
-#include "example.h"
+#include <CallInvokerHolder.h>
+#include <fbjni/fbjni.h>
+#include <jsi/jsi.h>
 
-extern "C"
-JNIEXPORT jint JNICALL
-Java_com_reactnativebbssignatures_BbsSignaturesModule_nativeMultiply(JNIEnv *env, jclass type, jint a, jint b) {
-    return example::multiply(a, b);
+#include "TurboUtilsModule.h"
+#include "Logging.h"
+
+using namespace facebook;
+
+// this reflects com.myturboutils.NativeProxy class
+struct NativeProxy : jni::JavaClass<NativeProxy> {
+    static constexpr auto kJavaDescriptor = "Lcom/reactnativebbssignatures/NativeProxy;";
+
+    static void registerNatives() {
+        // register native methods for Java
+        javaClassStatic()->registerNatives({makeNativeMethod("installNativeJsi", NativeProxy::installNativeJsi)});
+    }
+
+private:
+    static void installNativeJsi(jni::alias_ref<jni::JObject> thiz,
+                                 jlong jsiRuntimePtr,
+                                 jni::alias_ref<react::CallInvokerHolder::javaobject> jsCallInvokerHolder) {
+
+        auto jsiRuntime = reinterpret_cast<jsi::Runtime*>(jsiRuntimePtr);
+        auto jsCallInvoker = jsCallInvokerHolder->cthis()->getCallInvoker();
+
+
+        // initialize turbo module
+        utils::installTurboModule(*jsiRuntime, jsCallInvoker);
+    }
+};
+
+JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void*) {
+    return jni::initialize(vm, [] {
+        NativeProxy::registerNatives();
+    });
 }
