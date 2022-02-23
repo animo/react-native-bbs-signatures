f0d57c1de5809c1e46fcb6942e1f5a2804d617e9
Berend Sliedrecht <berend@animo.id>
2022-02-23 13:26:32 +0100

    generated cpp code from the interface
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- lib/cpp-generated/NativeModules.h
+++ lib/cpp-generated/NativeModules.h
@@ -16,7 +16,212 @@ namespace react {
 class JSI_EXPORT BbsSignaturesCxxSpecJSI : public TurboModule {
 public:
   BbsSignaturesCxxSpecJSI(std::shared_ptr<CallInvoker> jsInvoker);
-  virtual double multiply(jsi::Runtime &rt, double a, double b) = 0;
+  virtual void bbs_string_free(jsi::Runtime &rt,
+                               const jsi::Object &options) = 0;
+  virtual void bbs_byte_buffer_free(jsi::Runtime &rt,
+                                    const jsi::Object &options) = 0;
+  virtual void free_bbs_blind_commitment(jsi::Runtime &rt,
+                                         const jsi::Object &options) = 0;
+  virtual double bbs_blind_signature_size(jsi::Runtime &rt,
+                                          const jsi::Object &options) = 0;
+  virtual double
+  bbs_blind_commitment_context_init(jsi::Runtime &rt,
+                                    const jsi::Object &options) = 0;
+  virtual double bbs_blind_commitment_context_add_message_string(
+      jsi::Runtime &rt, const jsi::Object &options) = 0;
+  virtual double bbs_blind_commitment_context_add_message_bytes(
+      jsi::Runtime &rt, const jsi::Object &options) = 0;
+  virtual double bbs_blind_commitment_context_add_message_prehashed(
+      jsi::Runtime &rt, const jsi::Object &options) = 0;
+  virtual double
+  bbs_blind_commitment_context_set_public_key(jsi::Runtime &rt,
+                                              const jsi::Object &options) = 0;
+  virtual double
+  bbs_blind_commitment_context_set_nonce_string(jsi::Runtime &rt,
+                                                const jsi::Object &options) = 0;
+  virtual double
+  bbs_blind_commitment_context_set_nonce_bytes(jsi::Runtime &rt,
+                                               const jsi::Object &options) = 0;
+  virtual double bbs_blind_commitment_context_set_nonce_prehashed(
+      jsi::Runtime &rt, const jsi::Object &options) = 0;
+  virtual double
+  bbs_blind_commitment_context_finish(jsi::Runtime &rt,
+                                      const jsi::Object &options) = 0;
+  virtual void free_bbs_blind_sign(jsi::Runtime &rt,
+                                   const jsi::Object &options) = 0;
+  virtual double bbs_blinding_factor_size(jsi::Runtime &rt,
+                                          const jsi::Object &options) = 0;
+  virtual double bbs_blind_sign_context_init(jsi::Runtime &rt,
+                                             const jsi::Object &options) = 0;
+  virtual double
+  bbs_blind_sign_context_add_message_string(jsi::Runtime &rt,
+                                            const jsi::Object &options) = 0;
+  virtual double
+  bbs_blind_sign_context_add_message_bytes(jsi::Runtime &rt,
+                                           const jsi::Object &options) = 0;
+  virtual double
+  bbs_blind_sign_context_add_message_prehashed(jsi::Runtime &rt,
+                                               const jsi::Object &options) = 0;
+  virtual double
+  bbs_blind_sign_context_set_public_key(jsi::Runtime &rt,
+                                        const jsi::Object &options) = 0;
+  virtual double
+  bbs_blind_sign_context_set_secret_key(jsi::Runtime &rt,
+                                        const jsi::Object &options) = 0;
+  virtual double
+  bbs_blind_sign_context_set_commitment(jsi::Runtime &rt,
+                                        const jsi::Object &options) = 0;
+  virtual double bbs_blind_sign_context_finish(jsi::Runtime &rt,
+                                               const jsi::Object &options) = 0;
+  virtual double bbs_unblind_signature(jsi::Runtime &rt,
+                                       const jsi::Object &options) = 0;
+  virtual void free_create_proof(jsi::Runtime &rt,
+                                 const jsi::Object &options) = 0;
+  virtual double bbs_create_proof_context_size(jsi::Runtime &rt,
+                                               const jsi::Object &options) = 0;
+  virtual double bbs_create_proof_context_init(jsi::Runtime &rt,
+                                               const jsi::Object &options) = 0;
+  virtual double bbs_create_proof_context_add_proof_message_string(
+      jsi::Runtime &rt, const jsi::Object &options) = 0;
+  virtual double bbs_create_proof_context_add_proof_message_bytes(
+      jsi::Runtime &rt, const jsi::Object &options) = 0;
+  virtual double bbs_create_proof_context_add_proof_message_prehashed(
+      jsi::Runtime &rt, const jsi::Object &options) = 0;
+  virtual double
+  bbs_create_proof_context_set_signature(jsi::Runtime &rt,
+                                         const jsi::Object &options) = 0;
+  virtual double
+  bbs_create_proof_context_set_public_key(jsi::Runtime &rt,
+                                          const jsi::Object &options) = 0;
+  virtual double
+  bbs_create_proof_context_set_nonce_string(jsi::Runtime &rt,
+                                            const jsi::Object &options) = 0;
+  virtual double
+  bbs_create_proof_context_set_nonce_bytes(jsi::Runtime &rt,
+                                           const jsi::Object &options) = 0;
+  virtual double
+  bbs_create_proof_context_set_nonce_prehashed(jsi::Runtime &rt,
+                                               const jsi::Object &options) = 0;
+  virtual double
+  bbs_create_proof_context_finish(jsi::Runtime &rt,
+                                  const jsi::Object &options) = 0;
+  virtual void free_bbs_sign(jsi::Runtime &rt, const jsi::Object &options) = 0;
+  virtual double bbs_signature_size(jsi::Runtime &rt,
+                                    const jsi::Object &options) = 0;
+  virtual double bbs_sign_context_init(jsi::Runtime &rt,
+                                       const jsi::Object &options) = 0;
+  virtual double
+  bbs_sign_context_add_message_string(jsi::Runtime &rt,
+                                      const jsi::Object &options) = 0;
+  virtual double
+  bbs_sign_context_add_message_bytes(jsi::Runtime &rt,
+                                     const jsi::Object &options) = 0;
+  virtual double
+  bbs_sign_context_add_message_prehashed(jsi::Runtime &rt,
+                                         const jsi::Object &options) = 0;
+  virtual double
+  bbs_sign_context_set_secret_key(jsi::Runtime &rt,
+                                  const jsi::Object &options) = 0;
+  virtual double
+  bbs_sign_context_set_public_key(jsi::Runtime &rt,
+                                  const jsi::Object &options) = 0;
+  virtual double bbs_sign_context_finish(jsi::Runtime &rt,
+                                         const jsi::Object &options) = 0;
+  virtual double bbs_verify_context_init(jsi::Runtime &rt,
+                                         const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_context_add_message_string(jsi::Runtime &rt,
+                                        const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_context_add_message_bytes(jsi::Runtime &rt,
+                                       const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_context_add_message_prehashed(jsi::Runtime &rt,
+                                           const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_context_set_public_key(jsi::Runtime &rt,
+                                    const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_context_set_signature(jsi::Runtime &rt,
+                                   const jsi::Object &options) = 0;
+  virtual double bbs_verify_context_finish(jsi::Runtime &rt,
+                                           const jsi::Object &options) = 0;
+  virtual void free_verify_proof(jsi::Runtime &rt,
+                                 const jsi::Object &options) = 0;
+  virtual double
+  bbs_get_total_messages_count_for_proof(jsi::Runtime &rt,
+                                         const jsi::Object &options) = 0;
+  virtual double bbs_verify_proof_context_init(jsi::Runtime &rt,
+                                               const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_proof_context_add_message_string(jsi::Runtime &rt,
+                                              const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_proof_context_add_message_bytes(jsi::Runtime &rt,
+                                             const jsi::Object &options) = 0;
+  virtual double bbs_verify_proof_context_add_message_prehashed(
+      jsi::Runtime &rt, const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_proof_context_set_proof(jsi::Runtime &rt,
+                                     const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_proof_context_set_public_key(jsi::Runtime &rt,
+                                          const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_proof_context_set_nonce_string(jsi::Runtime &rt,
+                                            const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_proof_context_set_nonce_bytes(jsi::Runtime &rt,
+                                           const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_proof_context_set_nonce_prehashed(jsi::Runtime &rt,
+                                               const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_proof_context_finish(jsi::Runtime &rt,
+                                  const jsi::Object &options) = 0;
+  virtual void free_verify_sign_proof(jsi::Runtime &rt,
+                                      const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_blind_commitment_context_init(jsi::Runtime &rt,
+                                           const jsi::Object &options) = 0;
+  virtual double bbs_verify_blind_commitment_context_add_blinded(
+      jsi::Runtime &rt, const jsi::Object &options) = 0;
+  virtual double bbs_verify_blind_commitment_context_set_public_key(
+      jsi::Runtime &rt, const jsi::Object &options) = 0;
+  virtual double bbs_verify_blind_commitment_context_set_nonce_string(
+      jsi::Runtime &rt, const jsi::Object &options) = 0;
+  virtual double bbs_verify_blind_commitment_context_set_nonce_bytes(
+      jsi::Runtime &rt, const jsi::Object &options) = 0;
+  virtual double bbs_verify_blind_commitment_context_set_nonce_prehashed(
+      jsi::Runtime &rt, const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_blind_commitment_context_set_proof(jsi::Runtime &rt,
+                                                const jsi::Object &options) = 0;
+  virtual double
+  bbs_verify_blind_commitment_context_finish(jsi::Runtime &rt,
+                                             const jsi::Object &options) = 0;
+  virtual double bls_secret_key_size(jsi::Runtime &rt,
+                                     const jsi::Object &options) = 0;
+  virtual double bls_public_key_g2_size(jsi::Runtime &rt,
+                                        const jsi::Object &options) = 0;
+  virtual double blinding_factor_size(jsi::Runtime &rt,
+                                      const jsi::Object &options) = 0;
+  virtual double bls_public_key_g1_size(jsi::Runtime &rt,
+                                        const jsi::Object &options) = 0;
+  virtual double bls_generate_g2_key(jsi::Runtime &rt,
+                                     const jsi::Object &options) = 0;
+  virtual double bls_generate_g1_key(jsi::Runtime &rt,
+                                     const jsi::Object &options) = 0;
+  virtual double bls_generate_blinded_g2_key(jsi::Runtime &rt,
+                                             const jsi::Object &options) = 0;
+  virtual double bls_generate_blinded_g1_key(jsi::Runtime &rt,
+                                             const jsi::Object &options) = 0;
+  virtual double bls_get_public_key(jsi::Runtime &rt,
+                                    const jsi::Object &options) = 0;
+  virtual double bls_secret_key_to_bbs_key(jsi::Runtime &rt,
+                                           const jsi::Object &options) = 0;
+  virtual double bls_public_key_to_bbs_key(jsi::Runtime &rt,
+                                           const jsi::Object &options) = 0;
 };
 
 } // namespace react
--- src/turboModule.ts
+++ src/turboModule.ts
@@ -1,93 +1,92 @@
 import type { TurboModule } from 'react-native-tscodegen-types'
 import { TurboModuleRegistry } from 'react-native-tscodegen-types'
-import type { int32_t, uint64_t } from './bbs'
 
 // General interface for the package. Our generated cpp wrapper will be based on this
 export interface BbsNativeBindings extends TurboModule {
   bbs_string_free(options: {}): void
   bbs_byte_buffer_free(options: {}): void
   free_bbs_blind_commitment(options: {}): void
-  bbs_blind_signature_size(options: {}): int32_t
-  bbs_blind_commitment_context_init(options: {}): uint64_t
-  bbs_blind_commitment_context_add_message_string(options: {}): int32_t
-  bbs_blind_commitment_context_add_message_bytes(options: {}): int32_t
-  bbs_blind_commitment_context_add_message_prehashed(options: {}): int32_t
-  bbs_blind_commitment_context_set_public_key(options: {}): int32_t
-  bbs_blind_commitment_context_set_nonce_string(options: {}): int32_t
-  bbs_blind_commitment_context_set_nonce_bytes(options: {}): int32_t
-  bbs_blind_commitment_context_set_nonce_prehashed(options: {}): int32_t
-  bbs_blind_commitment_context_finish(options: {}): int32_t
+  bbs_blind_signature_size(options: {}): number
+  bbs_blind_commitment_context_init(options: {}): number
+  bbs_blind_commitment_context_add_message_string(options: {}): number
+  bbs_blind_commitment_context_add_message_bytes(options: {}): number
+  bbs_blind_commitment_context_add_message_prehashed(options: {}): number
+  bbs_blind_commitment_context_set_public_key(options: {}): number
+  bbs_blind_commitment_context_set_nonce_string(options: {}): number
+  bbs_blind_commitment_context_set_nonce_bytes(options: {}): number
+  bbs_blind_commitment_context_set_nonce_prehashed(options: {}): number
+  bbs_blind_commitment_context_finish(options: {}): number
   free_bbs_blind_sign(options: {}): void
-  bbs_blinding_factor_size(options: {}): int32_t
-  bbs_blind_sign_context_init(options: {}): uint64_t
-  bbs_blind_sign_context_add_message_string(options: {}): int32_t
-  bbs_blind_sign_context_add_message_bytes(options: {}): int32_t
-  bbs_blind_sign_context_add_message_prehashed(options: {}): int32_t
-  bbs_blind_sign_context_set_public_key(options: {}): int32_t
-  bbs_blind_sign_context_set_secret_key(options: {}): int32_t
-  bbs_blind_sign_context_set_commitment(options: {}): int32_t
-  bbs_blind_sign_context_finish(options: {}): int32_t
-  bbs_unblind_signature(options: {}): int32_t
+  bbs_blinding_factor_size(options: {}): number
+  bbs_blind_sign_context_init(options: {}): number
+  bbs_blind_sign_context_add_message_string(options: {}): number
+  bbs_blind_sign_context_add_message_bytes(options: {}): number
+  bbs_blind_sign_context_add_message_prehashed(options: {}): number
+  bbs_blind_sign_context_set_public_key(options: {}): number
+  bbs_blind_sign_context_set_secret_key(options: {}): number
+  bbs_blind_sign_context_set_commitment(options: {}): number
+  bbs_blind_sign_context_finish(options: {}): number
+  bbs_unblind_signature(options: {}): number
   free_create_proof(options: {}): void
-  bbs_create_proof_context_size(options: {}): int32_t
-  bbs_create_proof_context_init(options: {}): uint64_t
-  bbs_create_proof_context_add_proof_message_string(options: {}): int32_t
-  bbs_create_proof_context_add_proof_message_bytes(options: {}): int32_t
-  bbs_create_proof_context_add_proof_message_prehashed(options: {}): int32_t
-  bbs_create_proof_context_set_signature(options: {}): int32_t
-  bbs_create_proof_context_set_public_key(options: {}): int32_t
-  bbs_create_proof_context_set_nonce_string(options: {}): int32_t
-  bbs_create_proof_context_set_nonce_bytes(options: {}): int32_t
-  bbs_create_proof_context_set_nonce_prehashed(options: {}): int32_t
-  bbs_create_proof_context_finish(options: {}): int32_t
+  bbs_create_proof_context_size(options: {}): number
+  bbs_create_proof_context_init(options: {}): number
+  bbs_create_proof_context_add_proof_message_string(options: {}): number
+  bbs_create_proof_context_add_proof_message_bytes(options: {}): number
+  bbs_create_proof_context_add_proof_message_prehashed(options: {}): number
+  bbs_create_proof_context_set_signature(options: {}): number
+  bbs_create_proof_context_set_public_key(options: {}): number
+  bbs_create_proof_context_set_nonce_string(options: {}): number
+  bbs_create_proof_context_set_nonce_bytes(options: {}): number
+  bbs_create_proof_context_set_nonce_prehashed(options: {}): number
+  bbs_create_proof_context_finish(options: {}): number
   free_bbs_sign(options: {}): void
-  bbs_signature_size(options: {}): int32_t
-  bbs_sign_context_init(options: {}): uint64_t
-  bbs_sign_context_add_message_string(options: {}): int32_t
-  bbs_sign_context_add_message_bytes(options: {}): int32_t
-  bbs_sign_context_add_message_prehashed(options: {}): int32_t
-  bbs_sign_context_set_secret_key(options: {}): int32_t
-  bbs_sign_context_set_public_key(options: {}): int32_t
-  bbs_sign_context_finish(options: {}): int32_t
-  bbs_verify_context_init(options: {}): uint64_t
-  bbs_verify_context_add_message_string(options: {}): int32_t
-  bbs_verify_context_add_message_bytes(options: {}): int32_t
-  bbs_verify_context_add_message_prehashed(options: {}): int32_t
-  bbs_verify_context_set_public_key(options: {}): int32_t
-  bbs_verify_context_set_signature(options: {}): int32_t
-  bbs_verify_context_finish(options: {}): int32_t
+  bbs_signature_size(options: {}): number
+  bbs_sign_context_init(options: {}): number
+  bbs_sign_context_add_message_string(options: {}): number
+  bbs_sign_context_add_message_bytes(options: {}): number
+  bbs_sign_context_add_message_prehashed(options: {}): number
+  bbs_sign_context_set_secret_key(options: {}): number
+  bbs_sign_context_set_public_key(options: {}): number
+  bbs_sign_context_finish(options: {}): number
+  bbs_verify_context_init(options: {}): number
+  bbs_verify_context_add_message_string(options: {}): number
+  bbs_verify_context_add_message_bytes(options: {}): number
+  bbs_verify_context_add_message_prehashed(options: {}): number
+  bbs_verify_context_set_public_key(options: {}): number
+  bbs_verify_context_set_signature(options: {}): number
+  bbs_verify_context_finish(options: {}): number
   free_verify_proof(options: {}): void
-  bbs_get_total_messages_count_for_proof(options: {}): int32_t
-  bbs_verify_proof_context_init(options: {}): uint64_t
-  bbs_verify_proof_context_add_message_string(options: {}): int32_t
-  bbs_verify_proof_context_add_message_bytes(options: {}): int32_t
-  bbs_verify_proof_context_add_message_prehashed(options: {}): int32_t
-  bbs_verify_proof_context_set_proof(options: {}): int32_t
-  bbs_verify_proof_context_set_public_key(options: {}): int32_t
-  bbs_verify_proof_context_set_nonce_string(options: {}): int32_t
-  bbs_verify_proof_context_set_nonce_bytes(options: {}): int32_t
-  bbs_verify_proof_context_set_nonce_prehashed(options: {}): int32_t
-  bbs_verify_proof_context_finish(options: {}): int32_t
+  bbs_get_total_messages_count_for_proof(options: {}): number
+  bbs_verify_proof_context_init(options: {}): number
+  bbs_verify_proof_context_add_message_string(options: {}): number
+  bbs_verify_proof_context_add_message_bytes(options: {}): number
+  bbs_verify_proof_context_add_message_prehashed(options: {}): number
+  bbs_verify_proof_context_set_proof(options: {}): number
+  bbs_verify_proof_context_set_public_key(options: {}): number
+  bbs_verify_proof_context_set_nonce_string(options: {}): number
+  bbs_verify_proof_context_set_nonce_bytes(options: {}): number
+  bbs_verify_proof_context_set_nonce_prehashed(options: {}): number
+  bbs_verify_proof_context_finish(options: {}): number
   free_verify_sign_proof(options: {}): void
-  bbs_verify_blind_commitment_context_init(options: {}): uint64_t
-  bbs_verify_blind_commitment_context_add_blinded(options: {}): int32_t
-  bbs_verify_blind_commitment_context_set_public_key(options: {}): int32_t
-  bbs_verify_blind_commitment_context_set_nonce_string(options: {}): int32_t
-  bbs_verify_blind_commitment_context_set_nonce_bytes(options: {}): int32_t
-  bbs_verify_blind_commitment_context_set_nonce_prehashed(options: {}): int32_t
-  bbs_verify_blind_commitment_context_set_proof(options: {}): int32_t
-  bbs_verify_blind_commitment_context_finish(options: {}): int32_t
-  bls_secret_key_size(options: {}): int32_t
-  bls_public_key_g2_size(options: {}): int32_t
-  blinding_factor_size(options: {}): int32_t
-  bls_public_key_g1_size(options: {}): int32_t
-  bls_generate_g2_key(options: {}): int32_t
-  bls_generate_g1_key(options: {}): int32_t
-  bls_generate_blinded_g2_key(options: {}): int32_t
-  bls_generate_blinded_g1_key(options: {}): int32_t
-  bls_get_public_key(options: {}): int32_t
-  bls_secret_key_to_bbs_key(options: {}): int32_t
-  bls_public_key_to_bbs_key(options: {}): int32_t
+  bbs_verify_blind_commitment_context_init(options: {}): number
+  bbs_verify_blind_commitment_context_add_blinded(options: {}): number
+  bbs_verify_blind_commitment_context_set_public_key(options: {}): number
+  bbs_verify_blind_commitment_context_set_nonce_string(options: {}): number
+  bbs_verify_blind_commitment_context_set_nonce_bytes(options: {}): number
+  bbs_verify_blind_commitment_context_set_nonce_prehashed(options: {}): number
+  bbs_verify_blind_commitment_context_set_proof(options: {}): number
+  bbs_verify_blind_commitment_context_finish(options: {}): number
+  bls_secret_key_size(options: {}): number
+  bls_public_key_g2_size(options: {}): number
+  blinding_factor_size(options: {}): number
+  bls_public_key_g1_size(options: {}): number
+  bls_generate_g2_key(options: {}): number
+  bls_generate_g1_key(options: {}): number
+  bls_generate_blinded_g2_key(options: {}): number
+  bls_generate_blinded_g1_key(options: {}): number
+  bls_get_public_key(options: {}): number
+  bls_secret_key_to_bbs_key(options: {}): number
+  bls_public_key_to_bbs_key(options: {}): number
 }
 
 // We MUST export this according to tscodegen. We are ignoring it however.
