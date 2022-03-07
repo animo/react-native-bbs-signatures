dd4f4663b6743311ad4f84e991cd1547587eae52
Berend Sliedrecht <berend@animo.id>
2022-03-07 15:12:29 +0100

    test: tests for the cpp wrapper part
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- cpp/bbs-signatures.h
+++ cpp/bbs-signatures.h
@@ -1,27 +1,23 @@
 #pragma once
 
-#include "bbs-key.h"
-#include "bbs.h"
-#include "blinded-bls-key-pair.h"
-#include "bls-key-pair.h"
+#include "include/bbs.h"
+#include "keys/bbs-key.h"
+#include "keys/blinded-bls-key-pair.h"
+#include "keys/bls-key-pair.h"
 #include <vector>
 
 class Bbs {
 public:
-  static ByteBuffer sign(ByteArray publicKey, ByteArray secretKey,
+  static ByteArray sign(ByteArray publicKey, ByteArray secretKey,
                          std::vector<ByteArray> messages, ExternError *err);
-  static void blsSign();
   static bool verify(ByteArray publicKey, ByteArray signature,
                      std::vector<ByteArray> messages, ExternError *err);
-  static void blsVerify();
   static ByteArray createProof(ByteArray nonce, ByteArray publicKey,
                                ByteArray signature,
                                std::vector<ByteArray> messages,
                                std::vector<int32_t> revealed, ExternError *err);
-  static void blsCreateProof();
   static bool verifyProof(ByteArray nonce, ByteArray publicKey, ByteArray proof,
                           std::vector<ByteArray> messages, ExternError *err);
-  static void blsVerifyProof();
   static std::tuple<ByteArray, ByteArray, ByteArray>
   commitmentForBlindSignRequest(ByteArray nonce, ByteArray publicKey,
                                 std::vector<ByteArray> messages,
@@ -32,7 +28,6 @@ public:
                                      ByteArray commitment,
                                      std::vector<int32_t> blinded,
                                      ExternError *err);
-  static void blindSign();
   static BlsKeyPair generateBls12381G1KeyPair(ByteArray seed, ExternError *err);
   static BlindedBlsKeyPair generateBlindedBls12381G1KeyPair(ByteArray seed,
                                                             ExternError *err);
---
+++ cpp/tests/README.md
@@ -0,0 +1,13 @@
+# Testing
+
+## Prerequisites
+
+1. cmake
+2. make
+3. clang
+
+4. build the ffi-bbs-signature [library](https://github.com/mattrglobal/ffi-bbs-signatures) with `cargo build --release`
+
+5. move the file in `target/release/*.dylib` to `cpp/tests/lib/*.dylib`
+
+6. run `./test`
